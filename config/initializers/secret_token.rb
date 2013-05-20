0f60b976fa4d40c324931f48329895b6ff53e21f
Thomas Welsh <thomas.r.welsh@gmail.com>
2013-05-20 13:34:20 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marco::Application.config.secret_token = 'cf15db9ec3a225501f00af10fe5007c263b26fe76c808c15244e19baf67504a40d8946290e881f7bb57ea1518023915e8c0934c4baf2775baf7f6244efe95bb2'
